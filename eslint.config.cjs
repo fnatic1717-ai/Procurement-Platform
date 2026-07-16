0510b5c39033d9fe85ba19448310ab1e328c18de
fnatic1717-ai <fnatic1717@gmail.com>
2026-07-16 22:48:17 +0300

    fix: align ESLint with TypeScript and JSX environments

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,2 +1,30 @@
 const js = require('@eslint/js');
-module.exports = [js.configs.recommended,{ignores:['dist/**','.next/**','coverage/**','node_modules/**']},{files:['**/*.{ts,tsx}'],languageOptions:{parser:require('@typescript-eslint/parser'),parserOptions:{project:false}},plugins:{'@typescript-eslint':require('@typescript-eslint/eslint-plugin')},rules:{'@typescript-eslint/no-unused-vars':['error',{argsIgnorePattern:'^_'}]}}];
+
+module.exports = [
+  js.configs.recommended,
+  {
+    ignores: ['dist/**', '.next/**', 'coverage/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: { project: false },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'none',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+];
